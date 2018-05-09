b4355e0fd5da3cfc2a1e72e9f453bc60cc7ebbcc
L-eru <l.eru.ninja@gmail.com>
2018-05-09 22:38:33 +0800

    Ubuntu安装MySQL5.7时不知到密码的情况下该如何重新设置

--- ubuntu/others.php
+++ ubuntu/others.php
@@ -5,3 +5,42 @@
 ```shell
 sudo apt-get install redis nginx mysql-server
 ```
+
+
+不知道MySQL5.7的密码
+--------------------
+
+1. 修改配置文件 /etc/mysql/mysql.conf.d/mysqld.cnf
+
+```
+# 在[mysqld]下一行添加
+skip-grant-tables
+```
+
+2. 重启mysql
+
+```shell
+$ sudo systemctl restart mysql
+```
+
+3. 进行mysql， 修改密码
+
+```
+$ mysql
+
+
+mysql> use mysql
+mysql> update mysql.user set authentication_string=password('mysql密码') where user='root' and Host ='localhost';  
+mysql> update user set plugin="mysql_native_password";
+mysql> flush privileges;
+mysql> quit;
+```
+
+4. 将第一步添加的配置信息删除
+
+
+5. 重启mysql服务器
+
+```shell
+$ sudo systemctl restart mysql
+```
